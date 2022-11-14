fix seeding in dataset and displacement normalisation

cardiac_motion/model/datasets.py
@@ -232,12 +232,13 @@ class CardiacMR_2D_Inference_UKBB(data.Dataset):
 
 
 class CardiacMR_2D_UKBB_SynthDeform(CardiacMR_2D_UKBB):
-    def __init__(self, *args, scales=(8, 16, 32), min_std=0.0, max_std=1.0, seed=None, **kwargs):
+    def __init__(self, *args, scales=(8, 16, 32), min_std=0.0, max_std=1.0, seed=None, norm_dvf=True, **kwargs):
         super().__init__(*args, **kwargs)
         self.scales = scales
         self.min_std = min_std
         self.max_std = max_std
-        self.rand = np.random.default_rng(seed)
+        self.norm_dvf = norm_dvf
+        self.np_rand = np.random.default_rng(seed)  # set by worker_init_fn() passed to dataloader
 
     @staticmethod
     def normalise_disp(disp):
@@ -325,8 +326,8 @@ class CardiacMR_2D_UKBB_SynthDeform(CardiacMR_2D_UKBB):
             sample_shape = np.int32(np.ceil(gen_shape / scale))
             sample_shape = (num_batch, ndims, *sample_shape)
 
-            std = self.rand.uniform(min_std, max_std, size=sample_shape)
-            gauss = self.rand.normal(0, std, size=sample_shape)
+            std = self.np_rand.uniform(min_std, max_std, size=sample_shape)
+            gauss = self.np_rand.normal(0, std, size=sample_shape)
             zoom = [o / s for o, s in zip(gen_shape, sample_shape[2:])]
             if scale > 1:
                 gauss = torch.from_numpy(gauss)
@@ -336,6 +337,9 @@ class CardiacMR_2D_UKBB_SynthDeform(CardiacMR_2D_UKBB):
         # integrate, upsample
         dvf = self.svf_exp(svf)
         dvf = F.interpolate(dvf, scale_factor=2, mode=zoom_mode, align_corners=True) * 2
+
+        if self.norm_dvf:
+            dvf = self.normalise_disp(dvf)
         return dvf
 
     def __getitem__(self, index):

cardiac_motion/train.py
@@ -4,6 +4,8 @@ import shutil
 import argparse
 import logging
 
+import numpy as np
+
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -16,6 +18,8 @@ from model.submodules import resample_transform
 
 from test import test
 from utils import xutils, flow_utils
+
+torch.manual_seed(7)
 
 
 def train_epoch(model, optimizer, dataloader, params, epoch, summary_writer):
@@ -46,7 +50,7 @@ def train_epoch(model, optimizer, dataloader, params, epoch, summary_writer):
 
             # loss
             mse_fn = torch.nn.MSELoss()
-            if args.supervised:
+            if args.supervised_synth:
                 dvf_gt = x_data["dvf"].squeeze(0).to(device=args.device)
                 loss = mse_fn(dvf, dvf_gt)
                 losses = {"mse_dvf": loss}
@@ -208,6 +212,14 @@ def train(model, optimizer, dataloaders, params):
     val_summary_writer.close()
 
 
+def seed_numpy_rng(worker_id):
+    """Pytorch dataloader worker_init_fn to seed numpy rng differently for each worker"""
+    worker_info = torch.utils.data.get_worker_info()
+    seed = worker_info.seed
+    dataset = worker_info.dataset
+    dataset.np_rand = np.random.default_rng(seed)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -297,6 +309,7 @@ if __name__ == "__main__":
         shuffle=False,
         num_workers=args.num_workers,
         pin_memory=args.cuda,
+        worker_init_fn=seed_numpy_rng,
     )
 
     val_dataset = CardiacMR_2D_Eval_UKBB(