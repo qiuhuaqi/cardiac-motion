fixing dvf scaling error during data loading

cardiac_motion/model/submodules.py
@@ -13,10 +13,16 @@ def relu():
     return nn.ReLU(inplace=True)
 
 
-def conv_block_1(in_channels, out_channels, kernel_size=3, stride=1, padding = 1, nonlinearity=relu):
+def conv_block_1(in_channels, out_channels, kernel_size=3, stride=1, padding=1, nonlinearity=relu):
     """Conv2d + Non-linearity + BN2d, Xavier initialisation"""
-    conv_layer = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                           padding=padding, bias=False)
+    conv_layer = nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        bias=False,
+    )
     nn.init.xavier_uniform(conv_layer.weight, gain=np.sqrt(2.0))
 
     nll_layer = nonlinearity()
@@ -30,7 +36,7 @@ def conv_block_1(in_channels, out_channels, kernel_size=3, stride=1, padding = 1
 
 def conv_blocks_2(in_channels, out_channels, strides=1):
     """Block of 2x Conv layers"""
-    conv1 = conv_block_1(in_channels, out_channels, stride = strides)
+    conv1 = conv_block_1(in_channels, out_channels, stride=strides)
     conv2 = conv_block_1(out_channels, out_channels, stride=1)
     layers = [conv1, conv2]
     return nn.Sequential(*layers)
@@ -38,18 +44,17 @@ def conv_blocks_2(in_channels, out_channels, strides=1):
 
 def conv_blocks_3(in_channels, out_channels, strides=1):
     """Block of 3x Conv layers"""
-    conv1 = conv_block_1(in_channels, out_channels, stride = strides)
+    conv1 = conv_block_1(in_channels, out_channels, stride=strides)
     conv2 = conv_block_1(out_channels, out_channels, stride=1)
     conv3 = conv_block_1(out_channels, out_channels, stride=1)
     layers = [conv1, conv2, conv3]
     return nn.Sequential(*layers)
 
 
-
 # ------------------------------------------- #
 # Spatial Transformer Modules
 # ------------------------------------------- #
-def resample_transform(source, offset, interp='bilinear'):
+def resample_transform(source, offset, interp="bilinear"):
     """
     Transform an image by sampling at coordinates on a deformed mesh grid.
 
@@ -84,13 +89,13 @@ def resample_transform(source, offset, interp='bilinear'):
     # each pair of coordinates on deformed grid is using x-y order,
     # i.e. (column_num, row_num)
     # as required by the the grid_sample() function
-    deformed_grid= torch.stack((grid_w, grid_h), 3)  # shape (N, H, W, 2)
-    deformed_image = F.grid_sample(source, deformed_grid, mode=interp)
+    deformed_grid = torch.stack((grid_w, grid_h), 3)  # shape (N, H, W, 2)
+    deformed_image = F.grid_sample(source, deformed_grid, mode=interp, align_corners=False)
 
     return deformed_image
 
 
-def resample_transform_cpu(source, offset, interp='bilinear'):
+def resample_transform_cpu(source, offset, interp="bilinear"):
     """
     Transform an image by sampling at coordinates on a deformed mesh grid. CPU version.
 
@@ -125,8 +130,7 @@ def resample_transform_cpu(source, offset, interp='bilinear'):
     # each pair of coordinates on deformed grid is using x-y order,
     # i.e. (column_num, row_num)
     # as required by the the grid_sample() function
-    deformed_grid= torch.stack((grid_w, grid_h), 3)  # shape (N, H, W, 2)
+    deformed_grid = torch.stack((grid_w, grid_h), 3)  # shape (N, H, W, 2)
     deformed_image = F.grid_sample(source, deformed_grid, mode=interp)
 
     return deformed_image
-

cardiac_motion/test.py
@@ -15,6 +15,7 @@ from model.dataset_utils import CenterCrop, Normalise, ToTensor
 from model.datasets import CardiacMR_2D_Eval_UKBB
 from utils.metrics import categorical_dice_stack, contour_distances_stack, detJac_stack, bending_energy_stack
 from utils import xutils
+from utils.visualise import visualise_result
 
 STRUCTURES = ["lv", "myo", "rv"]
 SEG_METRICS = ["dice", "mcd", "hd"]
@@ -32,6 +33,8 @@ def test(
     run_eval=True,
     save_output=False,
     save_metric_results=False,
+    log_visual_tb=False,
+    summary_writer=None,
     device=torch.device("cpu"),
 ):
     """Run model inference on test dataset"""
@@ -63,12 +66,12 @@ def test(
             if run_inference:
                 # run model inference
                 with torch.no_grad():
-                    dvf = model(image_ed_batch, image_es_batch)
+                    dvf_pred = model(image_ed_batch, image_es_batch)
                     if save_output:
                         test_output_dir_subj = f"{test_output_dir}/{dataloader.dataset.dir_list[idx]}"
                         if not os.path.exists(test_output_dir_subj):
                             os.makedirs(test_output_dir_subj)
-                        dvf_save = dvf.detach().cpu().numpy()
+                        dvf_save = dvf_pred.detach().cpu().numpy()
                         np.save(f"{test_output_dir_subj}/dvf.npy", dvf_save)
             else:
                 # load saved output from disk
@@ -77,15 +80,29 @@ def test(
                 ), f"Test output dir {test_output_dir} doesn't exist, have you run inference? "
                 test_output_dir_subj = f"{test_output_dir}/{dataloader.dataset.dir_list[idx]}"
                 dvf_loaded = np.load(f"{test_output_dir_subj}/dvf.npy")
-                dvf = torch.from_numpy(dvf_loaded).to(device)
+                dvf_pred = torch.from_numpy(dvf_loaded).to(device)
 
             # transform label mask of ES frame
-            warped_label_es_batch = resample_transform(label_es_batch.float(), dvf, interp="nearest")
+            warped_label_es_batch = resample_transform(label_es_batch.float(), dvf_pred, interp="nearest")
 
             # Move data to cpu and numpy
             warped_label_es_batch = warped_label_es_batch.squeeze(1).cpu().numpy().transpose(1, 2, 0)  # (H, W, N)
             label_ed_batch = label_ed_batch.squeeze(0).numpy().transpose(1, 2, 0)  # (H, W, N)
-            dvf = dvf.data.cpu().numpy().transpose(0, 2, 3, 1)  # (N, H, W, 2)
+            dvf = dvf_pred.data.cpu().numpy().transpose(0, 2, 3, 1)  # (N, H, W, 2)
+
+            # visualise in Tensorboard
+            if log_visual_tb:
+                warped_source = resample_transform(image_es_batch, dvf_pred)
+                vis_data_dict = {
+                    "target": image_ed_batch.cpu().numpy(),
+                    "source": image_es_batch.cpu().numpy(),
+                    "target_original": image_es_batch.cpu().numpy(),
+                    "target_pred": warped_source.cpu().numpy(),
+                    "warped_source": warped_source.cpu().numpy(),
+                    "disp_pred": dvf.transpose(0, 3, 1, 2) * image_ed_batch.shape[-1] / 2,
+                }
+                fig = visualise_result(vis_data_dict)
+                summary_writer.add_figure("val_test_fig", fig, close=True)
 
             if run_eval:
                 if not all_slices:

cardiac_motion/train.py
@@ -18,11 +18,12 @@ from model.submodules import resample_transform
 
 from test import test
 from utils import xutils, flow_utils
+from utils.visualise import visualise_result
 
 torch.manual_seed(7)
 
 
-def train_epoch(model, optimizer, dataloader, params, epoch, summary_writer):
+def train_epoch(model, optimizer, dataloader, args, params, epoch, summary_writer):
     """
     Train the model for one epoch.V
 
@@ -83,37 +84,55 @@ def train_epoch(model, optimizer, dataloader, params, epoch, summary_writer):
 
             # save visualisation of training results
             if (epoch + 1) % params.save_result_epochs == 0 or (epoch + 1) == params.num_epochs:
-                if it == len(dataloader) - 1:
-
+                if it == len(dataloader) - 1:  # at the end of the epoch
                     # warp source image with full resolution dvf
                     warped_source = resample_transform(source, dvf)
 
                     # [dvf and warped source] -> cpu -> numpy array
                     dvf_np = dvf.data.cpu().numpy().transpose(0, 2, 3, 1)  # (N, H, W, 2)
+                    dvf_np *= target.shape[-1] / 2
                     warped_source = warped_source.data.cpu().numpy()[:, 0, :, :] * 255  # (N, H, W)
 
                     # [input images] -> cpu -> numpy array -> [0, 255]
                     target = target.data.cpu().numpy()[:, 0, :, :] * 255  # (N, H, W)
                     source = source.data.cpu().numpy()[:, 0, :, :] * 255  # (N, H, W), here N = frames -1
 
-                    # set up the result dir for this epoch
-                    save_result_dir = os.path.join(args.model_dir, "train_results", "epoch_{}".format(epoch + 1))
-                    if not os.path.exists(save_result_dir):
-                        os.makedirs(save_result_dir)
+                    # # set up the result dir for this epoch
+                    # save_result_dir = os.path.join(args.model_dir, "train_results", "epoch_{}".format(epoch + 1))
+                    # if not os.path.exists(save_result_dir):
+                    #     os.makedirs(save_result_dir)
+                    #
+                    # # NOTE: the following code saves all N frames in a batch
+                    # # save dvf (hsv + quiver), target, source, warped source and error
+                    # # flow_utils.save_flow_hsv(op_flow, target, save_result_dir, fps=params.fps)
+                    # flow_utils.save_warp_n_error(warped_source, target, source, save_result_dir, fps=params.fps)
+                    # flow_utils.save_flow_quiver(
+                    #     dvf_np,
+                    #     source,
+                    #     save_result_dir,
+                    #     fps=params.fps,
+                    # )
 
-                    # NOTE: the following code saves all N frames in a batch
-                    # save dvf (hsv + quiver), target, source, warped source and error
-                    # flow_utils.save_flow_hsv(op_flow, target, save_result_dir, fps=params.fps)
-                    flow_utils.save_warp_n_error(warped_source, target, source, save_result_dir, fps=params.fps)
-                    flow_utils.save_flow_quiver(
-                        dvf_np * (target.shape[-1] / 2),
-                        source,
-                        save_result_dir,
-                        fps=params.fps,
+                    # visualise in Tensorboard
+                    vis_data_dict = {
+                        "target": target[:, np.newaxis, ...],
+                        "source": source[:, np.newaxis, ...],
+                        "target_original": source[:, np.newaxis, ...],
+                        "target_pred": warped_source[:, np.newaxis, ...],
+                        "warped_source": warped_source[:, np.newaxis, ...],
+                        "disp_pred": dvf_np.transpose(0, 3, 1, 2),
+                    }
+
+                    if "dvf" in x_data.keys():
+                        vis_data_dict["disp_gt"] = x_data["dvf"].squeeze(0).numpy() * target.shape[-1] / 2
+
+                    train_fig = visualise_result(data_dict=vis_data_dict)
+                    summary_writer.add_figure(
+                        "training_fig", train_fig, global_step=epoch * len(dataloader) + it, close=True
                     )
 
 
-def train(model, optimizer, dataloaders, params):
+def train(model, optimizer, dataloaders, args, params):
     """
     Train the model and evaluate every epoch.
 
@@ -145,7 +164,7 @@ def train(model, optimizer, dataloaders, params):
 
         # train the model for one epoch
         logging.info("Training...")
-        train_epoch(model, optimizer, train_dataloader, params, epoch, train_summary_writer)
+        train_epoch(model, optimizer, train_dataloader, args, params, epoch, train_summary_writer)
 
         # validation
         if (epoch + 1) % params.val_epochs == 0 or (epoch + 1) == params.num_epochs:
@@ -158,6 +177,8 @@ def train(model, optimizer, dataloaders, params):
                 save_output=False,
                 run_eval=True,
                 save_metric_results=False,
+                log_visual_tb=True,
+                summary_writer=val_summary_writer,
                 device=args.device,
             )
 
@@ -335,5 +356,5 @@ if __name__ == "__main__":
 
     # run
     logging.info("Starting training and validation for {} epochs.".format(params.num_epochs))
-    train(model, optimizer, dataloaders, params)
+    train(model, optimizer, dataloaders, args, params)
     logging.info("Training and validation complete.")